ec08ebd2e93ebd103f7f802a6c4651d108e2ab70
Cindy <c.yee1418@gmail.com>
2017-11-26 18:06:07 -0800

    Added a heap data structure

--- CMPE130_proj/Store_data.py
+++ CMPE130_proj/Store_data.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 15 20:51:09 2017
+Created on Sun Nov 26 17:46:20 2017
 
+@author: KuroX
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 20:51:09 2017
 @author: Cindy Yee
 """
 class Laptop(object):
@@ -10,6 +16,8 @@ class Laptop(object):
         self.model=model;
         self.price=price;
         self.release_date=release_date;
+    def __cmp__(self, other):               #used so we can sort into heaps
+        return cmp(self.price, other.price)
 
 CompanyA=[];
 
@@ -17,13 +25,29 @@ CompanyA=[];
 CompanyA.append(Laptop("Dell intel", "DELL123", float("399.99"), "6-12-2017"));
 CompanyA.append(Laptop("HP amd", "HP123", float("499.99"), "7-4-2017"));
 CompanyA.append(Laptop("Lenovo sss", "LVNVNV123", float("299.99"), "7-4-2014"));
-print "name:", CompanyA[0].name, "model:", CompanyA[0].model;
-print "price:",CompanyA[0].price, "release date:", CompanyA[0].release_date;
 
-#compare price
-if CompanyA[0].price<CompanyA[1].price:
-    print "1st cost less than 2nd"
-if CompanyA[1].price<CompanyA[2].price:
-    print "2nd cost less than 3rd"
-else:
-    print "2nd cost more than 3rd"    
+print "\nOriginal:"
+i=0;
+for iL in CompanyA:
+    print "name:", CompanyA[i].name, "model:", CompanyA[i].model;
+    print "\tprice:",CompanyA[i].price, "release date:", CompanyA[i].release_date;
+    i=i+1;
+
+##compare price
+#if CompanyA[0].price<CompanyA[1].price:
+#    print "1st cost less than 2nd"
+#if CompanyA[1].price<CompanyA[2].price:
+#    print "2nd cost less than 3rd"
+#else:
+#    print "2nd cost more than 3rd"    
+    
+print "\nHeapified "
+    
+import heapq
+
+heapq.heapify(CompanyA);
+j=0;
+for jL in CompanyA:
+   print "name:", CompanyA[j].name, "model:", CompanyA[j].model;
+   print "\tprice:",CompanyA[j].price, "release date:", CompanyA[j].release_date;
+   j=j+1; 
